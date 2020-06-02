db43f3ae82fe63cee49ea8b997851c637dce7ce2
root <root@ip-172-31-59-102.ec2.internal>
2020-06-02 04:49:49 +0000

    Mysw

---
+++ application/settings/database.php
@@ -0,0 +1,16 @@
+<?php defined('_ENGINE') or die('Access Denied'); return array (
+  'adapter' => 'mysqli',
+  'params' => 
+  array (
+    'host' => 'database-1-instance-1.cz9qo08x1aic.us-east-1.rds.amazonaws.com',
+    'username' => 'admin',
+    'password' => 'Ahead#123',
+    'dbname' => 'mydb',
+    'charset' => 'UTF8',
+    'adapterNamespace' => 'Zend_Db_Adapter',
+    'port' => NULL,
+  ),
+  'isDefaultTableAdapter' => true,
+  'tablePrefix' => 'engine4_',
+  'tableAdapterClass' => 'Engine_Db_Table',
+); ?>
